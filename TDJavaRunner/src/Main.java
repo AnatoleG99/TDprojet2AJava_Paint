b79866304382c213019d45e5a6f05832ff5436a6
anato <anatoleguyozot35@gmail.com>
2023-11-22 09:52:24 +0100

    TD2

--- TDJavaRunner/src/Main.java
+++ TDJavaRunner/src/Main.java
@@ -1,6 +1,15 @@
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main extends Application{
+public class Main extends Application {
 
         @Override
         public void start(Stage primaryStage) throws Exception{
@@ -16,5 +25,10 @@ public class Main extends Application{
 
         public static void main(String[] args) {
         launch(args);
+        Image spriteSheet = new Image("img\\heros.png");
+        ImageView sprite = new ImageView(spriteSheet);
+        sprite.setViewport(new Rectangle2D(20,0,65,100));
+        sprite.setX(200);
+        sprite.setY(300);
         }
 }
